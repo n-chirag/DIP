4329631f35f21487b52b7efe7b13e729fefe0826
n-chirag <narsariachirag@gmail.com>
2023-03-04 19:39:39 +0530

    deleted unnecessary folder

--- 4.7.0/search/variables_19.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['z',['z',['../d4/db4/classcv_1_1DualQuat.html#ad26885c85d72c4b9bc42d229b76e3790',1,'cv::DualQuat::z()'],['../da/d4a/classcv_1_1Quat.html#a6836f50dd74292ad753a51a906200af2',1,'cv::Quat::z()'],['../df/d6c/classcv_1_1Point3__.html#a1af9c182772a3c082e505f50eb5d2ee1',1,'cv::Point3_::z()']]],
-  ['z_5f',['z_',['../d4/db4/classcv_1_1DualQuat.html#a38eccd00b69906fa7c72a7a81b8f5c0e',1,'cv::DualQuat']]],
-  ['zeropoints',['zeropoints',['../d0/d5b/classcv_1_1dnn_1_1QuantizeLayer.html#ab02724956d681e24f7272f6eb3b7848e',1,'cv::dnn::QuantizeLayer::zeropoints()'],['../d5/d6a/classcv_1_1dnn_1_1DequantizeLayer.html#a6313c3a0409e15d5206aa2b73ddfc528',1,'cv::dnn::DequantizeLayer::zeropoints()']]]
-];
